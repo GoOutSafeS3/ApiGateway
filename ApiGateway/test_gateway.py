cdf3976cb782695dc991f5465f8f64102ac43cbf
GBisi <giuseppe.bisicchia@outlook.it>
2020-11-21 11:08:14 +0100

    Add tests

--- ApiGateway/test_gateway.py
+++ ApiGateway/test_gateway.py
@@ -31,4 +31,43 @@ class BookingsTests(unittest.TestCase):
         client = self.app.test_client()
         response = client.get('/bookings')
         js = response.get_json()
-        self.assertEquals(len(js),0,msg=js)
+        self.assertEqual(response.status_code,200,msg=js)
+        self.assertEqual(len(js),6,msg=js)
+
+    def test_get_booking(self):
+        client = self.app.test_client()
+        response = client.get('/bookings/1')
+        js = response.get_json()
+        self.assertEqual(response.status_code,200,msg=js)
+        self.assertEqual(js["id"],1,msg=js)
+
+    def test_get_bookings_with_users(self):
+        client = self.app.test_client()
+        response = client.get('/bookings?with_user=true')
+        js = response.get_json()
+        self.assertEqual(response.status_code,200,msg=js)
+        self.assertEqual(len(js),6,msg=js)
+        for b in js:
+            self.assertEqual(b["user_id"],b["user"]["id"],msg=b)
+
+    def test_get_booking_with_user(self):
+        client = self.app.test_client()
+        response = client.get('/bookings/1?with_user=true')
+        js = response.get_json()
+        self.assertEqual(response.status_code,200,msg=js)
+        self.assertEqual(js["id"],1,msg=js)
+        self.assertEqual(js["user_id"],js["user"]["id"],msg=js)
+
+    """
+    def test_delete_booking(self):
+        # Need to create a new booking first
+        client = self.app.test_client()
+        response = client.delete('/bookings/1')
+        js = response.get_json()
+        self.assertEqual(response.status_code,204,msg=js)
+        
+        response = client.get('/bookings/1')
+        js = response.get_json()
+        self.assertEqual(response.status_code,404,msg=js)
+    
+    """
